a782f0e6992f66a33232299eb76d198b49b4cfbb
huge200890 <https://huge200890@github.com>
2019-03-22 14:43:14 -0500

    Update distance training library

--- jie_development_packages/DNCON4_Distance_experiment/ResNet_arch/scripts/lewis/old_20190309/cal_Relu2D_weighted_MSE_inloop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-
-job=$(sbatch run_sbatch_gpu_2D_V100_Relu2D_weightedMSE_loop.sh)
-jid="$(cut -d " " -f4 <<<$job)"
-for i in $(seq 1 30)
-do
-job=$(sbatch --dependency=afterany:$jid run_sbatch_gpu_2D_V100_Relu2D_weightedMSE_loop.sh)
-jid="$(cut -d " " -f4 <<<$job)"
-done
